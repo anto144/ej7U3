6c9a0ab42dd2704e1bd4c4234781dca454a31ce8
anto144 <104173968+anto144@users.noreply.github.com>
2022-06-06 03:49:44 -0300

    Add files via upload

---
+++ 7/InterfazLista.py
@@ -0,0 +1,13 @@
+from zope.interface import Interface
+from zope.interface import implementer
+
+class ILista(Interface):
+
+    def insertarAparato(elemento,posicion):
+        pass
+    
+    def agregarAparato(elemento):
+        pass
+
+    def mostrarAparato(posicion):
+        pass
